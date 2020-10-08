ca1fb4c2f0d96f61679238d1dfc3bce1f3b45a7f
Dilrose Reji <70878223+dilroseR@users.noreply.github.com>
2020-10-08 16:23:30 +0530

    updated main.php

--- main.php
+++ main.php
@@ -8,19 +8,29 @@
     <a class="nav-link active" href="main.php">Home</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Why give blood</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Why give organ</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
+    <a class="nav-link" href="#">Become a donor</a>
+  </li> 
+  </ul>
+
+  <div class="jumbotron">
+  <h1 class="display-4">Be a Hero!</h1><br>
+  <p class="lead">You're</p>
+  <p class="lead">Somebody's</p>
+  <p class="lead">Type</p>
+  <hr class="my-4">
+  <p class="type"><b>DONATE BLOOD, DONATE ORGANS!</b></p>
+  <p class="type"><b> SAVE LIVES!</b></p>
+</div>
 
 
- 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+ <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>   
 </body>
