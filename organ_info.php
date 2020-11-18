a88a5d4eae1ebf3d4e0d6d9a0257b508b3147564
jenjam007 <70909864+jenjam007@users.noreply.github.com>
2020-11-18 14:15:54 +0530

    Update organ_info.php

--- organ_info.php
+++ organ_info.php
@@ -1,6 +1,6 @@
 <html><head><title>    </title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<link rel="stylesheet" href="css/organ_info.css">
+<link rel="stylesheet" href="organ_info.css">
 </head>
 <body>
 <ul class="nav">
@@ -29,11 +29,71 @@
   </ul>
 
   <div class="container">
-    <p>In need of organs?  <a href="patient-organ_form.php"><strong>Click here</strong></a></p>
+    <p class="lists"><h2><b>What is organ donation and transplantation?</b></h2></p> 
+    <p class="lists">Organ donation is the process of surgically removing an organ or tissue from one person (the organ donor) and placing it into another person (the recipient). Transplantation is necessary because the recipient’s organ has failed or has been damaged by disease or injury.</p>
+    <p class="lists"><h4><b>Who can be an organ donor?</p></h4></b>
+    <p class="lists">People of all ages should consider themselves potential donors. When a person dies, he or she is evaluated for donor suitability based on their medical history and age.</p>
+    
+    <p class="lists"><h4><b>Benefits of organ donation: </b><br><h5><strong><u>For the recipient:-</h5></strong></p></h4></u>
+    <ul class="lists">
+      <li><strong>Quality of life:</strong> Transplants can greatly improve a recipient’s health and quality of life, allowing them to return to normal activities. They can spend more time with family and friends, be more physically active, and pursue their interests more fully.</li>
+      <li><strong>Shorter waiting time: </strong> Due to the lack of organs available for transplant, patients on the national transplant list often face long wait times (sometimes several years) before they are able to receive a transplant from a deceased donor. Patients who find a suitable living donor do not have to wait on the list.</li>
+    </ul>
+    <p class="lists"><h5><strong><u>For the living donor:-</h5></p></strong></u>
+      <li><strong>Positive emotional experiences:</strong>The gift of an organ can save the life of a transplant candidate. The experience of providing this special gift to a person in need can serve be a positive aspect of donation.</li>
+      <li><strong>More time with your loved one:</strong>Donating an organ can increase the time you have to spend with your loved one as well as the quality of that time.</li>    
+    </ul>
+
+    <p class="lists"><h4><b>What organs can I donate?</p></h4></b>
+    
+    <div class="card" style="width: 18rem;">
+      <img src="images/kidney.jpg" class="card-img-kidney" alt="kidney">
+      <div class="card-body">
+        <p class="lists">On average, a living donor kidney can function anywhere between 12 to 20 years, and a deceased donor kidney can improve quality of life for 8 to 12 years.</p>
+        </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img src="images/heart.jpg" class="card-img-heart" alt="heart">
+      <div class="card-body">
+        <p class="lists">Donors give years of life to heart recipients and their families. There is an 85% survival rate at one year post heart surgery.</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="images/pancreas.jpg" class="card-img-pancreas" alt="pancreas">
+      <div class="card-body">
+        <p class="lists">Pancreas creates insulin to control your sugar levels and the amount of sugar in your blood, and releases the nutrients in your food that keep you healthy.</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img src="images/liver.jpg" class="card-img-liver" alt="liver">
+      <div class="card-body">
+        <p class="lists">Living donation is possible because the liver is the only organ that can regenerate itself.</p>
+        </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img src="images/intestine.jpg" class="card-img-intestine" alt="intestine">
+      <div class="card-body">
+        <p class="lists">Intestine donations are lifesaving. Donors can give years of life to intestinal transplant recipients.</p>
+        </div>
+    </div>
+
+<br>
+<br>
+<br>
+<div class="footer">
+  <p class="lists">In need of organs?  <a href="patient-organ_form.php"><strong>Click here</strong></a></p>
+
+</div>
+
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>   
-</div>
 </body>
+</div>
 </html>
